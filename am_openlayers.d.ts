56d1cf1b6a9f6b95824534858e7f95fdf42592b2
ArcelorGIS <cedric.corthout.external@arcelormittal.com>
2018-07-06 15:14:10 +0200

    Update am_openlayers.d.ts

--- am_openlayers.d.ts
+++ am_openlayers.d.ts
@@ -49,6 +49,35 @@ interface IRoadSearchService {
         clearEndPersoon(): void;
 }
 
+interface INewRoadSearchService {
+        getDisplayData(): any;
+        getStartPoint(): any;
+        getEndPoint(): any;
+        getParameters(): any;
+        afterStartPointSelect(data: any, event: any): void;
+        afterEndPointSelect(data: any, event: any): void;
+        startSearchNoExtent(projection?: string): void;
+        startSearch(projection?: string): void;
+        getPosten(isStart: boolean): Array<any>;
+        getPoorten(isStart: boolean): Array<any>;
+        getGebouwen(isStart: boolean): Array<any>;
+        getGebouwByNummer(nummer: string, postvak?: string): ng.IPromise<any>;
+        getPersonen(isStart: boolean): Array<any>;
+        setPoint(x: number, y: number, startOrEnd: boolean, geometry?: string, projection?: string): void;
+        clearMarker(startOrEnd: boolean): void;
+        clearSearch(): void;
+        clearRoads(): void;
+        resetDisplayData(): void;
+        clearStartPoort(): void;
+        clearEndPoort(): void;
+        clearStartPost(): void;
+        clearEndPost(): void;
+        clearStartGebouw(): void;
+        clearEndGebouw(): void;
+        clearStartPersoon(): void;
+        clearEndPersoon(): void;
+}
+
 interface IDataTableService {
     getTabs(): Array<any>;
     getTab(tabName: string): any;
